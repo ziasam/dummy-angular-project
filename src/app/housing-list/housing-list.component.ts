ae6460f9b4914bc2f05cabdc2562421c9b74f038
Ziauddin Sameer <ziauddinsameer@gmail.com>
2023-05-17 16:58:02 +0600

    implemented search funtion

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -9,6 +9,7 @@ import { HousingLoaction } from '../housing-loaction';
 export class HousingListComponent  implements OnInit{
 
   @Input() locationList: HousingLoaction[] = [];
+  results:HousingLoaction[] = [];
   constructor() {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -16,6 +17,7 @@ export class HousingListComponent  implements OnInit{
 
   searchLocations(search: string) {
     console.log(search);
-
+    if(!search) return;
+    this.results = this.locationList.filter(location => location.city.toLowerCase().includes(search.toLowerCase()));
   }
 }
